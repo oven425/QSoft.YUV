16f75292980caa0dcef4b0be266d881b1f37c4d9
oven425 <oven425@yahoo.com.tw>
2023-06-01 18:03:28 +0800

    update 1. add new colorspace project

---
+++ QSoft.ColorSpace/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QSoft.ColorSpace
+{
+    public class Class1
+    {
+    }
+
+    public enum ColorSpaces
+    {
+        YUY2,
+        RGB24,
+        RGB32
+    }
+    public class ColorSpaceBase
+    {
+
+    }
+
+    public static class ColorSpace
+    {
+        public static byte[] Rotate(this byte[] src)
+        {
+            return null;
+        }
+    }
+
+    
+}
